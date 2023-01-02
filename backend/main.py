19861b903f057db84b051d46bcb98dc81b79c5e3
Axel-Martin Tammekand <axelmartintammekand@gmail.com>
2023-01-02 20:32:52 +0200

    added create user

--- backend/main.py
+++ backend/main.py
@@ -22,6 +22,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 # --------------------- Models --------------------- #
 
 # Token models
+
 class Token(BaseModel):
     access_token: str
     token_type: str
@@ -32,6 +33,26 @@ class TokenData(BaseModel):
 
 
 # User models
+
+class User(BaseModel):
+    id: int | None = None
+    master_id: int | None = None
+    role: str | None = None
+    username: str | None = None
+    password: str | None = None
+    email: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    phone: str | None = None
+    address: str | None = None
+    assigned_hotel_id: int | None = None
+    create_permission: bool | None = None
+    read_permission: bool | None = None
+    update_permission: bool | None = None
+    delete_permission: bool | None = None
+
+    class Config:
+        orm_mode = True
 
 
 # Hotel models
@@ -183,10 +204,42 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 # --------------------- Users --------------------- #
 
 
+# Create user
+@app.post("/users/create")
+async def create_user(user: User, db: Session = Depends(get_db)):
+    try:
+        user.id = (0 if db.query(func.max(models.Users.id)).scalar(
+        ) == None else db.query(func.max(models.Users.id)).scalar()) + 1
+        db.add(models.Users(
+            id=user.id,
+            master_id=user.master_id if user.master_id else None,
+            role=user.role,
+            username=user.username,
+            hashed_password=get_password_hash(user.password),
+            email=user.email,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            phone=user.phone,
+            address=user.address,
+            assigned_hotel_id=user.assigned_hotel_id,
+            create_permission=int(user.create_permission),
+            read_permission=int(user.read_permission),
+            update_permission=int(user.update_permission),
+            delete_permission=int(user.delete_permission)
+        ))
+
+        db.commit()
+
+        return db.query(models.Users).filter(models.Users.id == user.id).first()
+    except Exception as e:
+        print(e)
+        return {"message": "Error: " + str(e)}
+
+
 # --------------------- Hotels --------------------- #
 
 # Get all hotels
-@app.get("/hotels/all", response_model=list[Hotel])
+@ app.get("/hotels/all", response_model=list[Hotel])
 async def get_all_hotels(db: Session = Depends(get_db)):
     hotels = db.query(models.Hotels).all()
 
@@ -204,7 +257,7 @@ async def get_all_hotels(db: Session = Depends(get_db)):
 
 
 # Get hotel by id
-@app.get("/hotels/{hotel_id}", response_model=Hotel)
+@ app.get("/hotels/{hotel_id}", response_model=Hotel)
 async def get_hotel_by_id(hotel_id: int, db: Session = Depends(get_db)):
     hotel = db.query(models.Hotels).filter(
         models.Hotels.id == hotel_id).first()
@@ -223,79 +276,84 @@ async def get_hotel_by_id(hotel_id: int, db: Session = Depends(get_db)):
 
 
 # Create hotel
-@app.post("/hotels/create")
+@ app.post("/hotels/create")
 async def create_hotel(hotel: Hotel, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
-    token_data = validate_token(token)
+    try:
+        token_data = validate_token(token)
 
-    # check if user has owner status
-    user = db.query(models.Users).filter(
-        models.Users.username == token_data.username).first()
-    if user.role != "owner":
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="You do not have 'owner' status to create a hotel",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        # check if user has owner status
+        user = db.query(models.Users).filter(
+            models.Users.username == token_data.username).first()
+        if user.role != "owner":
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You do not have 'owner' status to create a hotel",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
 
-    # take highest id and add 1
-    hotel.id = db.query(func.max(models.Hotels.id)).scalar() + 1
+        # take highest id and add 1
+        hotel.id = db.query(func.max(models.Hotels.id)).scalar() + 1
 
-    # create hotel
-    db.add(models.Hotels(
-        id=hotel.id,
-        owner_id=user.id,
-        name=hotel.name,
-        story_count=hotel.story_count,
-        stars=hotel.stars,
-        address=hotel.address,
-        phone=hotel.phone,
-        email=hotel.email,
-        website=hotel.website,
-        description=hotel.description,
-    ))
+        # create hotel
+        db.add(models.Hotels(
+            id=hotel.id,
+            owner_id=user.id,
+            name=hotel.name,
+            story_count=hotel.story_count,
+            stars=hotel.stars,
+            address=hotel.address,
+            phone=hotel.phone,
+            email=hotel.email,
+            website=hotel.website,
+            description=hotel.description,
+        ))
 
-    # create amenities
-    if hotel.amenities_in:
-        for amenity in hotel.amenities_in:
-            db.add(models.HotelAmenities(
-                hotel_id=hotel.id,
-                type="in",
-                name=amenity.amenity,
-            ))
-    if hotel.amenities_out:
-        for amenity in hotel.amenities_out:
-            db.add(models.HotelAmenities(
-                hotel_id=hotel.id,
-                type="out",
-                name=amenity.amenity,
-            ))
-    if hotel.pictures:
-        for picture in hotel.pictures:
-            db.add(models.HotelPictures(
-                hotel_id=hotel.id,
-                url=picture.url,
-            ))
+        # create amenities
+        if hotel.amenities_in:
+            for amenity in hotel.amenities_in:
+                db.add(models.HotelAmenities(
+                    hotel_id=hotel.id,
+                    type="in",
+                    name=amenity.amenity,
+                ))
+        if hotel.amenities_out:
+            for amenity in hotel.amenities_out:
+                db.add(models.HotelAmenities(
+                    hotel_id=hotel.id,
+                    type="out",
+                    name=amenity.amenity,
+                ))
+        if hotel.pictures:
+            for picture in hotel.pictures:
+                db.add(models.HotelPictures(
+                    hotel_id=hotel.id,
+                    url=picture.url,
+                ))
 
-    db.commit()
+        db.commit()
 
-    if hotel.rooms:
-        for room in hotel.rooms:
-            room.id = db.query(func.max(models.Rooms.id)).scalar() + 1
-            db.add(models.Rooms(
-                room_id=room.id,
-                hotel_id=hotel.id,
-                type=room.name,
-                price=room.price,
-                bed_count=room.bed_count,
-                ext_bed_count=room.ext_bed_count,
-                room_number=room.room_number,
-            ))
+        if hotel.rooms:
+            for room in hotel.rooms:
+                room.id = db.query(func.max(models.Rooms.id)).scalar() + 1
+                db.add(models.Rooms(
+                    room_id=room.id,
+                    hotel_id=hotel.id,
+                    type=room.name,
+                    price=room.price,
+                    bed_count=room.bed_count,
+                    ext_bed_count=room.ext_bed_count,
+                    room_number=room.room_number,
+                ))
 
-            if room.amenities:
-                for amenity in room.amenities:
-                    db.add(models.RoomAmenities(
-                        room_id=room.id,
-                        name=amenity.amenity,
-                    ))
+                if room.amenities:
+                    for amenity in room.amenities:
+                        db.add(models.RoomAmenities(
+                            room_id=room.id,
+                            name=amenity.amenity,
+                        ))
 
-            db.commit()
+                db.commit()
+
+        return {"message": "Hotel created"}
+    except Exception as e:
+        return {"message": "Error: " + str(e)}
