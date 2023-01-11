64fb1d40299a9cc16c8a7e874b7e3c10bb7919fa
Axel-Martin Tammekand <axelmartintammekand@gmail.com>
2023-01-11 16:14:34 +0200

    frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -24,13 +24,31 @@ import {
 	MDBModalTitle,
 	MDBModalBody,
 	MDBModalFooter,
+	MDBRating,
 } from "mdb-react-ui-kit";
 import { BrowserRouter as Router } from "react-router-dom";
 
 export default function App() {
 	//login popup
-	const [basicModal, setBasicModal] = useState(false);
-	const toggleShow = () => setBasicModal(!basicModal);
+	const [loginModal, setloginModal] = useState(false);
+	const toggleShowLogin = () => setloginModal(!loginModal);
+
+	//Create Hotel popup
+	const [createModal, setCreateModal] = useState(false);
+	const toggleShowCreate = () => setCreateModal(!createModal);
+
+	//Create hotel data
+	const [hotelName, setHotelName] = useState(null);
+	const [hotelStoryCount, setHotelStoryCount] = useState(null);
+	const [hotelStars, setHotelStars] = useState(null);
+	const [hotelAddress, setHotelAddress] = useState(null);
+	const [hotelPhone, setHotelPhone] = useState(null);
+	const [hotelEmail, setHotelEmail] = useState(null);
+	const [hotelWebsite, setHotelWebsite] = useState(null);
+	const [hotelDescription, setHotelDescription] = useState(null);
+	const [hotelAmenitiesIn, setHotelAmenitiesIn] = useState([]);
+	const [hotelAmenitiesOut, setHotelAmenitiesOut] = useState([]);
+	const [hotelImage, setHotelImage] = useState([]);
 
 	//if isLoggedIn and username
 	const [fullname, setFullname] = useState(null);
@@ -57,7 +75,7 @@ export default function App() {
 		} else {
 			setIsLoggedIn(false);
 		}
-	}, [toggleShow]);
+	}, [toggleShowLogin]);
 
 	//Login input
 	const [username, setUsername] = useState(null);
@@ -88,12 +106,37 @@ export default function App() {
 			.catch((err) => console.error(err));
 
 		console.log("Requesting Token");
-		toggleShow();
+		toggleShowLogin();
 	};
 
 	const logOut = () => {
 		localStorage.removeItem("token");
 		setIsLoggedIn(false);
+	};
+
+	var hotelData = {
+		name: hotelName,
+		story_count: hotelStoryCount,
+		stars: hotelStars,
+		address: hotelAddress,
+		phone: hotelPhone,
+		email: hotelEmail,
+		website: hotelWebsite,
+		description: hotelDescription,
+		amenities_in: hotelAmenitiesIn,
+		amenities_out: hotelAmenitiesOut,
+		image: hotelImage,
+	};
+
+	const createHotel = async (token) => {
+		await fetch("http://localhost:8000/hotels/create", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify(hotelData),
+		});
 	};
 
 	return (
@@ -121,17 +164,27 @@ export default function App() {
 						<MDBBtn outline>Search</MDBBtn>
 					</MDBInputGroup>
 					{isLoggedIn == false && (
-						<MDBBtn onClick={toggleShow}>Login</MDBBtn>
+						<MDBBtn className="mb-0 me-4" onClick={toggleShowLogin}>
+							Login
+						</MDBBtn>
 					)}
 					{isLoggedIn && (
-						<div className="d-flex justify-content-center align-items-center h-100">
+						<div className="d-flex align-items-center">
 							<p className="text-white mb-0 me-4">{fullname}</p>
-							<MDBBtn onClick={logOut}>Log Out</MDBBtn>
+							<MDBBtn className="mb-0 me-4" onClick={logOut}>
+								Log Out
+							</MDBBtn>
+							<MDBBtn
+								className="mb-0 me-4"
+								onClick={toggleShowCreate}
+							>
+								Create
+							</MDBBtn>
 						</div>
 					)}
 				</MDBContainer>
 			</MDBNavbar>
-			<MDBModal show={basicModal} setShow={setBasicModal} tabIndex="-1">
+			<MDBModal show={loginModal} setShow={setloginModal} tabIndex="-1">
 				<MDBModalBody>
 					<MDBContainer fluid>
 						<MDBRow className="d-flex justify-content-center align-items-center h-100">
@@ -147,7 +200,7 @@ export default function App() {
 										<MDBBtn
 											className="btn-close mt-3"
 											color="light"
-											onClick={toggleShow}
+											onClick={toggleShowLogin}
 										></MDBBtn>
 									</div>
 
@@ -187,7 +240,11 @@ export default function App() {
 											className="mt-4 mb-5 mx-2 px-5"
 											size="lg"
 											onClick={() =>
-												fetchToken(username, password)
+												createHotel(
+													localStorage.getItem(
+														"token"
+													)
+												)
 											}
 										>
 											Login
@@ -203,6 +260,191 @@ export default function App() {
 												</a>
 											</p>
 										</div>
+									</MDBCardBody>
+								</MDBCard>
+							</MDBCol>
+						</MDBRow>
+					</MDBContainer>
+				</MDBModalBody>
+			</MDBModal>
+			<MDBModal show={createModal} setShow={setCreateModal} tabIndex="-1">
+				<MDBModalBody>
+					<MDBContainer fluid>
+						<MDBRow className="d-flex justify-content-center align-items-center h-100">
+							<MDBCol col="12">
+								<MDBCard
+									className="bg-dark text-white my-5 mx-auto"
+									style={{
+										borderRadius: "1rem",
+										maxWidth: "75%",
+									}}
+								>
+									<div className="p-2 mx-2">
+										<MDBBtn
+											className="btn-close mt-3"
+											color="light"
+											onClick={toggleShowCreate}
+										></MDBBtn>
+									</div>
+
+									<MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
+										<h2 className="fw-bold mb-2 text-uppercase">
+											Create Hotel
+										</h2>
+										<p className="text-white-50 mb-5">
+											Please enter your hotel's
+											description
+										</p>
+
+										<MDBInput
+											className="text-white"
+											wrapperClass="mb-4 mx-5 w-100"
+											labelClass="text-white"
+											label="Hotel's Name"
+											type="text"
+											size="lg"
+											onChange={(e) =>
+												setHotelName(e.target.value)
+											}
+										/>
+
+										<MDBInput
+											className="text-white"
+											wrapperClass="mb-4 mx-5 w-100"
+											labelClass="text-white"
+											label="Story Count"
+											type="text"
+											size="lg"
+											onChange={(e) =>
+												setHotelStoryCount(
+													e.target.value
+												)
+											}
+										/>
+
+										<MDBInput
+											className="text-white"
+											wrapperClass="mb-4 mx-5 w-100"
+											labelClass="text-white"
+											label="Stars (1-5)"
+											type="text"
+											size="lg"
+											onChange={(e) =>
+												setHotelStars(e.target.value)
+											}
+										/>
+
+										<MDBInput
+											className="text-white"
+											wrapperClass="mb-4 mx-5 w-100"
+											labelClass="text-white"
+											label="Address"
+											type="text"
+											size="lg"
+											onChange={(e) =>
+												setHotelAddress(e.target.value)
+											}
+										/>
+
+										<MDBInput
+											className="text-white"
+											wrapperClass="mb-4 mx-5 w-100"
+											labelClass="text-white"
+											label="Phone Number"
+											type="text"
+											size="lg"
+											onChange={(e) =>
+												setHotelPhone(e.target.value)
+											}
+										/>
+
+										<MDBInput
+											className="text-white"
+											wrapperClass="mb-4 mx-5 w-100"
+											labelClass="text-white"
+											label="Hotel's Email"
+											type="text"
+											size="lg"
+											onChange={(e) =>
+												setHotelPhone(e.target.value)
+											}
+										/>
+
+										<MDBInput
+											className="text-white"
+											wrapperClass="mb-4 mx-5 w-100"
+											labelClass="text-white"
+											label="Hotel's Website"
+											type="text"
+											size="lg"
+											onChange={(e) =>
+												setHotelWebsite(e.target.value)
+											}
+										/>
+
+										<MDBInput
+											className="text-white"
+											wrapperClass="mb-4 mx-5 w-100"
+											labelClass="text-white"
+											label="Hotel's Description"
+											type="text"
+											size="lg"
+											onChange={(e) =>
+												setHotelDescription(
+													e.target.value
+												)
+											}
+										/>
+
+										<MDBInput
+											className="text-white"
+											wrapperClass="mb-4 mx-5 w-100"
+											labelClass="text-white"
+											label="Hotel's Amenities In"
+											type="text"
+											size="lg"
+											onChange={(e) =>
+												setHotelAmenitiesIn(
+													e.target.value
+												)
+											}
+										/>
+
+										<MDBInput
+											className="text-white"
+											wrapperClass="mb-4 mx-5 w-100"
+											labelClass="text-white"
+											label="Hotel's Amenities Out"
+											type="text"
+											size="lg"
+											onChange={(e) =>
+												setHotelAmenitiesOut(
+													e.target.value
+												)
+											}
+										/>
+
+										<MDBInput
+											className="text-white"
+											wrapperClass="mb-4 mx-5 w-100"
+											labelClass="text-white"
+											label="Hotel's Image (URL)"
+											type="text"
+											size="lg"
+											onChange={(e) =>
+												setHotelImage(e.target.value)
+											}
+										/>
+
+										<MDBBtn
+											className="mt-4 mb-5 mx-2 px-5"
+											size="lg"
+											onClick={() =>
+												fetchToken(username, password)
+											}
+										>
+											Create
+										</MDBBtn>
 									</MDBCardBody>
 								</MDBCard>
 							</MDBCol>
