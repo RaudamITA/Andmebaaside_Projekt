8b5f4bfde6752b466ed515d5d392eae42ae25b30
Axel-Martin Tammekand <axelmartintammekand@gmail.com>
2023-11-08 13:29:59 +0200

    fix log message

--- backend/src/endpoints/users.py
+++ backend/src/endpoints/users.py
@@ -41,13 +41,13 @@ async def create_user(user: UserIn, db: Session = Depends(get_db)):
         response = db.query(Users.username).filter(
             Users.id == user.id).first()
         
-        await log(user.username, 'Login for access token', '201', db=db)
+        await log(user.username, 'Create user', '201', db=db)
         return {
             "message": "User " + response[0] + " created successfully", "user_id": user.id,
             "status_code": status.HTTP_201_CREATED
         }
     except Exception as e:
-        await log(user.username, 'Login for access token', '400', db=db)
+        await log(user.username, 'Create user', '400', db=db)
         return {
             "message": "Error: " + str(e),
             "status_code": status.HTTP_400_BAD_REQUEST
